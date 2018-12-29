b7970935b20f1759af1a7fa81027f47eb4240dc2
Albert <maxiaoscut@aliyun.com>
2018-12-29 17:56:04 +0800

    Add files via upload

---
+++ UnSupervised-Mahalanobis Distance/mahal_dist.py
@@ -0,0 +1,21 @@
+# Author：马肖
+# E-mail：maxiaoscut@aliyun.com
+# Github：https://github.com/Albertsr
+
+def mahal_dist(matrix):
+    matrix_mean = np.mean(matrix, axis=0)
+    delta = matrix - matrix_mean
+    
+    # 求协方差矩阵及其逆矩阵
+    cov_matrix = np.cov(matrix, rowvar=False, ddof=1)
+    cov_matrix_inv = LA.inv(cov_matrix)  
+
+    # 求单个样本向量与样本中心的马氏距离
+    def md_vector(vector):        
+        inner_prod = np.dot(vector, cov_matrix_inv)
+        inner_product = np.dot(inner_prod, vector)
+        dist = np.sqrt(inner_product)
+        return dist
+    
+    mahal_dist = np.apply_along_axis(arr=delta, axis=1, func1d=md_vector)
+    return mahal_dist
---
+++ UnSupervised-Mahalanobis Distance/mahal_dist_variant.py
@@ -0,0 +1,24 @@
+# Author：马肖
+# E-mail：maxiaoscut@aliyun.com
+# Github：https://github.com/Albertsr
+
+import numpy as np
+from numpy import linalg as LA
+from sklearn.preprocessing import StandardScaler 
+
+
+def mahal_dist_variant(matrix):
+    matrix = StandardScaler().fit_transform(matrix)
+    cov_matrix = np.cov(matrix, rowvar=False, ddof=1)
+    eigen_values, eigen_vectors = LA.eig(cov_matrix)
+        
+    # 函数get_score用于返回训练集每一个样本在特定主成分上的分数
+    # 参数pc_idx表示主成分的索引
+    def get_score(pc_idx):
+        # eigen_vectors.T[pc_idx]表示第idx个主成分构成的列向量
+        inner_product = np.dot(matrix, eigen_vectors.T[pc_idx])
+        score = np.square(inner_product) / eigen_values[pc_idx]
+        return score
+    # 返回训练集每一个样本在所有主成分上的分数，并分别求和
+    mahal_dist = sum(map(get_score, range(len(eigen_values))))
+    return mahal_dist
