f601f6d1e8f3f45000690ffe18d2c53950aec258
Albert <maxiaoscut@aliyun.com>
2019-01-05 19:42:00 +0800

    Update mahal_dist_variant.py

--- UnSupervised-Mahalanobis Distance/mahal_dist_variant.py
+++ UnSupervised-Mahalanobis Distance/mahal_dist_variant.py
@@ -8,11 +8,13 @@ from sklearn.preprocessing import StandardScaler
 
 
 def mahal_dist_variant(matrix):
+    # 将数据集标准化
     matrix = StandardScaler().fit_transform(matrix)
+    # 对数据集进行主成分分析
     cov_matrix = np.cov(matrix, rowvar=False, ddof=1)
     eigen_values, eigen_vectors = LA.eig(cov_matrix)
         
-    # 函数get_score用于返回训练集每一个样本在特定主成分上的分数
+    # 函数get_score用于返回数据集在单个主成分上的分数
     # 参数pc_idx表示主成分的索引
     def get_score(pc_idx):
         # eigen_vectors.T[pc_idx]表示第idx个主成分构成的列向量
