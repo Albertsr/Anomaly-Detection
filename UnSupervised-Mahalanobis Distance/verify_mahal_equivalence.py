6d7abeb3d669d3c78e5d8527de4373d8f222426e
Albert <maxiaoscut@aliyun.com>
2019-01-05 14:51:34 +0800

    Add files via upload

---
+++ UnSupervised-Mahalanobis Distance/verify_mahal_equivalence.py
@@ -0,0 +1,32 @@
+# Author：马肖
+# E-mail：maxiaoscut@aliyun.com
+# Github：https://github.com/Albertsr
+
+import numpy as np
+from mahal_dist import mahal_dist
+from mahal_dist_variant import mahal_dist_variant
+
+
+def verify_maldist_equivalence(seed, row=1000, col=50):
+    rdg = np.random.RandomState(seed)
+    matrix = rdg.randn(row, col)
+    
+    # 马氏距离的初始定义
+    dist_original = mahal_dist(matrix)
+    indices_desc_original = np.argsort(-dist_original)
+    
+    # 马氏距离的变体
+    dist_variant = mahal_dist_variant(matrix)
+    indices_desc_variant = np.argsort(-dist_variant)
+    
+    assert not np.allclose(dist_original, dist_variant), '马氏距离及其变体返回的数值一般不相等'
+    verify_indices = np.allclose(indices_desc_original, indices_desc_variant)
+    return verify_indices
+
+# 生成一系列随机种子
+seeds = np.random.choice(range(1000), size=10, replace=False)
+# 返回验证结果
+verify_result = list(map(verify_maldist_equivalence, seeds))
+if all(verify_result):
+    description = '经过{:}个不重复的随机数据集的测试，马氏距离及其变体对样本相对异常程度的评估是一致的'
+    print(description.format(len(seeds)))
