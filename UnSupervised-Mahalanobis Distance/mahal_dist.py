7066a634102d26010966349b2a50906c32a5a084
Albert <maxiaoscut@aliyun.com>
2019-01-05 19:41:42 +0800

    Update mahal_dist.py

--- UnSupervised-Mahalanobis Distance/mahal_dist.py
+++ UnSupervised-Mahalanobis Distance/mahal_dist.py
@@ -7,7 +7,9 @@ from numpy import linalg as LA
 
 
 def mahal_dist(matrix):
+    # 计算样本矩阵的中心向量
     matrix_mean = np.mean(matrix, axis=0)
+    # 计算各样本与中心向量之间的差异
     delta = matrix - matrix_mean
     
     # 求协方差矩阵及其逆矩阵
