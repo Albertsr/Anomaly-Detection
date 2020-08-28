e52a1d9a09343f4c1c8eca25a9f44e0ae572ec75
Ma Xiao <maxiaoscut@aliyun.com>
2020-08-28 15:45:15 +0800

    Update ADOA.py

--- SemiSupervised-ADOA/ADOA.py
+++ SemiSupervised-ADOA/ADOA.py
@@ -1,4 +1,4 @@
-# Author：马肖
+# Author：MaXiao
 # E-mail：maxiaoscut@aliyun.com
 # Github：https://github.com/Albertsr
 
@@ -8,140 +8,130 @@ from cluster_centers import get_cluster_centers
 from sklearn.preprocessing import StandardScaler, minmax_scale
 
 class ADOA:
-    def __init__(self, anomalies, unlabel, classifer, return_proba=True, n_clusters='auto', cluster_algo='kmeans', 
-                 contamination=0.02, theta=0.85, alpha='auto', beta='auto', random_state=2018):
-        scaler = StandardScaler()
-        scaled_dataset = scaler.fit_transform(anomalies.append(unlabel))
-        self.anomalies = scaled_dataset.iloc[:len(anomalies), :] 
-        self.unlabel = scaled_dataset.iloc[len(anomalies):, :] 
-        self.n_clusters = n_clusters # 聚类簇数可以预先指定，也可以由get_cluster_centers自动确定最佳聚类簇数
-        self.classifer = classifer # 选取的分类器
-        self.return_proba = return_proba # 布尔型参数，是否返回样本取正的后验概率
-        self.cluster_algo = cluster_algo # 聚类算法，可以选取'spectral'、'birch'、'dbscan'、'kmeans'，默认取'kmeans'
-        self.contamination = contamination # contamination为预估的U中异常样本(即正样本)比例
-        self.theta = theta # isolation_score、similarity_score的加权系数分别为theta、1-theta
-        self.alpha = alpha # 论文默认取已知异常样本(P集)的total_score均值，作为无标签样本是否为potential anomalies的阈值
-        self.beta = beta # 判定无标签样本是否为reliable normal的阈值
-        self.random_state = random_state
-        self.centers = get_cluster_centers(self.anomalies, self.n_clusters, self.cluster_algo) # 返回聚类的簇中心
-    
-    # 返回算法在给定数据集下生成的基本信息
-    def __repr__(self):
-        info_1 = '1) The positives(observed_anomalies) is divided into {:} clusters.\n'
-        info_2 = '2) The thresholds : alpha = {:.6f}, beta = {:.6f}\n'
-        info_3 = "3) reliable normal's number = {:}, potential anomalies's number = {:}\n"
-        info_4 = "4) The final negative's number = {:}, positive's number = {:}"
-        info = (info_1 + info_2 + info_3 + info_4)
-        a, _, b, _ = self.classify_unlabel()
-        y_train = self.weighted_trainset()[1]
-        return info.format(len(self.centers), self.alpha, self.beta, len(a), len(b),\
-                           np.sum(y_train==0), np.sum(y_train==1))
-    
-    # 返回数据集中所有样本的total_score
-    def get_total_score(self):
-        dataset = np.r_[self.anomalies, self.unlabel]
+    """Implementation of ADOA (Anomaly Detection with Partially Observed Anomalies)"""
+    def __init__(self, anomalies, unlabel, classifer, cluster_algo='kmeans', n_clusters='auto', 
+                 contamination=0.01, theta=0.85, alpha='auto', beta='auto', return_proba=False, 
+                 random_state=2018):
+        """
+        Parameters
+        --------------------------
+        - anomalies : 
+              Observed anomaly data sets
+              
+        - unlabel: 
+              Unlabeled data sets.
         
-        # 计算isolation_score
-        iforest = IsolationForest(n_estimators=120, contamination=self.contamination, 
-                                  random_state=self.random_state, n_jobs=-1, behaviour="new")
-        iforest.fit(dataset)  
-        # ADOA论文原文：The higher is the score IS(x) (close to 1), the more likely that x being an anomaly
-        # Sklearn document：score_samples(X): Opposite of the anomaly score defined in the original paper.
-        isolation_score = -iforest.score_samples(dataset)  # 负号不能丢
- 
-        # 计算similarity_score
-        def get_similarity_score(x, centers=self.centers):
-            # 计算样本与P集各簇中心的最短距离，再计算最终的similarity score，且两者成反比
-            min_dist = np.min([np.square(x - center).sum() for center in centers])   
-            '''
-            计算similarity_score时，论文原文中min_dist没有除以特征数，但实际数据表明：当特征数较多时，
-            即使数据已经标准化，自然底数e的指数幂构成的分母也可能极大，similarity_score的最大值也接近于0，
-            缺乏加权意义，而除以特征数self.anomalies.shape[1]有助于缓解此现象，且不影响距离的相对大小关系
-            '''
-            similarity_score = np.exp(-min_dist/self.anomalies.shape[1])
-            return similarity_score
-        similarity_score = np.array(list(map(get_similarity_score, dataset)))
-        # 对similarity_score进一步MIN_MAX标准化，与isolation_score保持一致的[0, 1]区间
-        similarity_score = minmax_scale(similarity_score)     
+        - classifer: 
+              A Classifer fitting weighted samples and labels to predict unlabel samples.
+              
+        - cluster_algo : str, {'kmeans'、'spectral'、'birch'、'dbscan'}, default = 'kmeans'
+              Clustering algorithm for clustering anomaly samples.      
+              
+        - n_clusters: int, default=5
+               The number of clusters to form as well as the number of centroids to generate.
         
-        # 计算total_score，对isolation_score应赋予较大的权重，self.theta默认取0.85
-        total_score = isolation_score * self.theta + similarity_score * (1-self.theta)
-        return total_score
-    
-    # 对无标签样本集U进行分类，从中筛选出potential anomalies、reliable normal
-    def classify_unlabel(self):
-        total_score = self.get_total_score()
-        # observed_anomalies_score、unlabel_scores分别为U集、P集对应的total_score
-        observed_anomalies_score = total_score[:len(self.anomalies)]
-        unlabel_scores = total_score[len(self.anomalies):]
-        
-        if self.alpha == 'auto' and self.beta == 'auto':
-            # 论文将alpha的取值默认设定为已知异常样本total_score的均值
-            self.alpha = np.mean(observed_anomalies_score)
-            
-            # beta为判定无标签样本是否为reliable normal samples的阈值
-            # 如果所有样本total_score的中位数np.median(total_score)小于alpha，则视其为beta，否则取45分位点处的值
-            if np.median(total_score) < self.alpha:
-                self.beta = np.median(total_score)
-            else:
-                self.beta = np.percentile(total_score, 45)
-            # beta的取值应小于alpha，否则输出warning
-            if self.beta > self.alpha:
-                warning = 'beta({:.6f}) is bigger than alpha({:.6f}), you should properly decrease the value of beta.'
-                print(warning.format(self.beta, self.alpha))
+        - contamination : float, range (0, 0.5).
+              The proportion of outliers in the data set. 
 
-        def clf(score):
-            if score >= self.alpha:
-                # 'potential anomalies'
-                return 1
-            elif score <= self.beta:
-                # 'reliable normal samples' 
-                return 0 
-            else:
-                return 0.5
-            
-        # clf_result为U中所有样本的分类结果
-        clf_result = np.array(list(map(clf, unlabel_scores)))
-        # 确定reliable_normal及其分数
-        reliable_normal = self.unlabel[clf_result==0]
-        reliable_normal_score = unlabel_scores[clf_result==0]
-        # 确定potential_anomalies及其分数
-        potential_anomalies = self.unlabel[clf_result==1]
-        potential_anomalies_score = unlabel_scores[clf_result==1]
-        return reliable_normal, reliable_normal_score, potential_anomalies, potential_anomalies_score
+        - theta : float, range [0, 1].
+              The weights of isolation_score and similarity_score are theta and 1-theta respectively.
+              
+        - alpha : float, should be positive number, default = mean value of anomalies's score
+              Threshold value for determining unlabel sample as potential anomaly
+              
+        - beta : float, should be positive number
+              Threshold value for determining unlabel sample as reliable normal sample
+
+        - return_proba : bool, default=False
+              Whether return the predicted probability for positive(anomaly) class for each sample.
+              Need classifer to provide predict_proba method.
+        """
+        dataset_scaled = StandardScaler().fit_transform(np.r_[anomalies, unlabel])
+        self.anomalies = dataset_scaled[:len(anomalies), :] 
+        self.unlabel = dataset_scaled[len(anomalies):, :] 
+        self.contamination = contamination
+        self.classifer = classifer 
+        self.n_clusters = n_clusters
+        self.cluster_algo = cluster_algo
+        self.theta = theta 
+        self.alpha = alpha 
+        self.beta = beta 
+        self.return_proba = return_proba 
+        self.random_state = random_state
+        self.centers, self.cluster_score = get_cluster_centers(self.anomalies, self.n_clusters, self.cluster_algo)
+    
+    def cal_weighted_score(self):
+        dataset = np.r_[self.anomalies, self.unlabel]
+        iforest = IsolationForest(n_estimators=100, contamination=self.contamination, 
+                                  random_state=self.random_state, n_jobs=-1)
+        iforest.fit(dataset)  
+        # Paper：The higher is the score IS(x) (close to 1), the more likely that x being an anomaly.
+        # Scikit-learn API : decision_function(X): The lower, the more abnormal.
+        isolation_score = -iforest.decision_function(dataset)  
+        isolation_score_scaled = minmax_scale(isolation_score)
         
-    def weighted_trainset(self): 
-        total_score = self.get_total_score()
-        reliable_normal, reliable_normal_score, potential_anomalies, potential_anomalies_score = self.classify_unlabel()
+        def cal_similarity_score(arr, centers=self.centers):
+            min_dist = np.min([np.square(arr - center).sum() for center in centers])
+            similarity_score = np.exp(-min_dist/len(arr))
+            '''
+            In the paper, when calculating similarity_score, min_dist is not divided by the number of features 
+            (col_num), but when the number of features is large, the value of np.exp(min_dist) is very large, 
+            so that similarity_score is close to 0, which lacks weighted meaning. Dividing by the number of 
+            features helps to alleviate this phenomenon and does not affect the ordering of similarity_score.  
+            '''
+            return similarity_score
+        similarity_score = [cal_similarity_score(arr) for arr in dataset]
+        similarity_score_scaled = minmax_scale(similarity_score)
+        weighted_score = self.theta * isolation_score_scaled *  + (1-self.theta) * similarity_score_scaled
+        return weighted_score
+    
+    def determine_trainset(self):
+        weighted_score = self.cal_weighted_score()
+        min_score, max_score, median_score = [func(weighted_score) for func in (np.min, np.max, np.median)]
+        anomalies_score = weighted_score[:len(self.anomalies)]
+        unlabel_scores = weighted_score[len(self.anomalies):]
         
-        # 求出total_score的最小与最大值
-        score_min, score_max = np.min(total_score), np.max(total_score)
-        # 设置reliable_normal的权重
-        reliable_normal_weight = (score_max-reliable_normal_score) / (score_max-score_min)
-        # 设置潜在异常样本(potential_anomalies)的权重
-        potential_anomalies_weight = potential_anomalies_score / score_max
-        # 将原有异常样本的权重全部设置为1
-        observed_anomalies_weight = np.ones(len(self.anomalies))
+        self.alpha = np.mean(anomalies_score) if self.alpha == 'auto' else self.alpha
+        self.beta = median_score if median_score < self.alpha else np.percentile(total_score, 45)
+        assert self.beta < self.alpha, 'beta should be smaller than alpha.'
         
-        # 生成训练数据：将已知的anomalies, potential_anomalies, reliable_normal在axis=0方向上予以整合
-        X_train = np.r_[self.anomalies, potential_anomalies, reliable_normal] 
-        # 生成权重
-        weights = np.r_[observed_anomalies_weight, potential_anomalies_weight, reliable_normal_weight]
+        # rlb:reliabel, ptt:potential
+        rlb_bool, ptt_bool = unlabel_scores<=self.beta, unlabel_scores>=self.alpha
+        rlb_normal, ptt_anomalies = self.unlabel[rlb_bool], self.unlabel[ptt_bool]
+        rlb_normal_score, ptt_anomalies_score = unlabel_scores[rlb_bool], unlabel_scores[ptt_bool]
+        rlb_normal_weight = (max_score-rlb_normal_score) / (max_score-min_score)
+        ptt_anomalies_weight = ptt_anomalies_score / max_score
         
-        # 生成标签：anomalies, potential_anomalies的标签均为1，reliable_normal的标签为0
-        observed_anomalies_label = observed_anomalies_weight
-        potential_anomalies_label = np.ones(len(potential_anomalies))
-        reliable_normal_label = np.zeros(len(reliable_normal))
-        y_train = np.r_[observed_anomalies_label, potential_anomalies_label, reliable_normal_label]
+        anomalies_weight = anomalies_label = np.ones(len(self.anomalies))
+        X_train = np.r_[self.anomalies, ptt_anomalies, rlb_normal] 
+        weights = np.r_[anomalies_weight, ptt_anomalies_weight, rlb_normal_weight]
+        y_train = np.r_[anomalies_label, np.ones(len(ptt_anomalies)), np.zeros(len(rlb_normal))].astype(int)
         return X_train, y_train, weights
     
     def predict(self):
-        X_train, y_train, weights = self.weighted_trainset()
+        X_train, y_train, weights = self.determine_trainset()
         clf = self.classifer
         clf.fit(X_train, y_train, sample_weight=weights)
         y_pred = clf.predict(self.unlabel)
         if self.return_proba:
-            y_prob = clf.predict_proba(self.unlabel)[:, -1]
+            y_prob = clf.predict_proba(self.unlabel)[:, 1]
             return y_pred, y_prob
         else:
             return y_pred
+        
+    def __repr__(self):
+        info_1 = \
+        '1) The Observed Anomalies is divided into {:} clusters, and the calinski_harabasz_score is {:.2f}.\n'.\
+        format(len(self.centers), self.cluster_score)
+        
+        y_train = self.determine_trainset()[1]
+        rll_num = np.sum(y_train==0)
+        ptt_num = sum(y_train)-len(self.anomalies)
+        
+        info_2 = "2) Reliable Normals's number = {:}, accounts for {:.2%} within the Unlabel dataset.\n".\
+        format(ptt_num, ptt_num/len(self.unlabel))
+        
+        info_3 = "3) Potential Anomalies's number = {:}, accounts for {:.2%} within the Unlabel dataset.".\
+        format(rll_num, rll_num/len(self.unlabel))
+        
+        return info_1 + info_2 + info_3
